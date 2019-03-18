ab43b4eb257cb002e1fd3a7ee09b7babd042a1d2
Felix <48635951+OnCodeDeny@users.noreply.github.com>
2019-03-18 04:58:14 -0400

    TEMP
    unfinished branch

---
+++ PrototypingWeek2/Assets/Scripts/FlagController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagController : MonoBehaviour
+{
+    public GameObject flagCloth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        flagCloth.gameObject.SetActive(false);
+    }
+
+}
