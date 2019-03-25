e97d657e67327d6ad27de24292e364d47e8d85b6
Liam <liampei@hotmail.com>
2019-03-25 19:48:53 -0400

    Grenades
    Project:
    - Added grenade prefab
    - Gave thickness to the floor to avoid grenades clipping through
    - Added grenade bounce physics material, located in the "Materials" folder
    - Added "Grenade.cs" into the "Scripts" folder
    
    GunController.cs:
    - Added switch to determine if weapon is projectile or not, and allowed weapon to have different functionality based on this.
    - Added "Grenade" gameobject reference
    
    Grenade.cs:
    - Added force to rigidbody upon object start.

---
+++ PrototypingWeek2/Assets/Scripts/Grenade.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float damage;
+    public float velocity;
+
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Debug.Log("Grenade spawned");
+        Debug.Log(damage);
+        rb.AddForce(transform.forward * velocity, ForceMode.Impulse);
+        transform.rotation = Random.rotation;
+    }
+
+    void Update()
+    {
+        
+    }
+}
