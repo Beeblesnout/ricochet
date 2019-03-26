ba05918bc83c149af3fd08bec69dc02bf261c8df
Loims <34040415+Loims@users.noreply.github.com>
2019-03-26 07:39:14 -0400

    Grenade Tinkers
    Grenade.cs:
    - Added countdown for grenade and deletes it when timer expires

--- PrototypingWeek2/Assets/Scripts/Grenade.cs
+++ PrototypingWeek2/Assets/Scripts/Grenade.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class Grenade : MonoBehaviour
 {
     private bool canExplode = false;
+    private bool hasExploded = false;
+
+    [SerializeField]
+    private SphereCollider explosionRadius;
+
+    [Space]
+    [SerializeField]
     private float explodeTimer;
+    private float timer;
     public float damage;
     public float velocity;
 
@@ -16,11 +24,36 @@ public class Grenade : MonoBehaviour
         Debug.Log(damage);
         rb.AddForce(transform.forward * velocity, ForceMode.Impulse);
         transform.rotation = Random.rotation;
+        timer = explodeTimer;
     }
 
     void Update()
     {
-        
+        if(hasExploded)
+        {
+            Destroy(this.gameObject);
+        }
+        switch(canExplode)
+        {
+            case true:
+                timer -= Time.deltaTime;
+                Debug.Log(timer);
+                if(timer <= 0)
+                {
+                    Debug.Log("THIS SHOULD EXPLODE");
+                    explosionRadius.enabled = true;
+                    hasExploded = true;
+                }
+                break;
+
+            case false:
+
+                break;
+
+            default:
+
+                break;
+        }
     }
 
     private void OnCollisionEnter(Collision other)
@@ -28,6 +61,15 @@ public class Grenade : MonoBehaviour
        if(other.gameObject.tag == "Environment" && !canExplode)
         {
             Debug.Log("OINGO BOINGO BROTHERS");
+            canExplode = true;
+        }
+    }
+
+    private void OnTriggerExit  (Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Damage");
         }
     }
 }
